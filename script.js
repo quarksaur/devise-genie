81d2a31cac7774fb0ad06235196dfa1246510018
IlanDeVinci <ilan.maouchi@gmail.com>
2024-09-22 14:33:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+// Api key for the exchange rate API
 const apiKey = "";
+
+// List of invalid characters for the input field
 const invalidChars = ["-", "e", "+", "E"];
 
+// Initialize the exchange rate list by looking at local storage or creating an empty list
 let exchangeRateList =
 	JSON.parse(localStorage.getItem("exchangeRateList")) || [];
 let currentExchangeRate;
+
 const amountInput = document.getElementById("amountInput");
 const amountOutput = document.getElementById("amountOutput");
 const currencySelectInput = document.getElementById("deviseInput");
 const currencySelectOutput = document.getElementById("deviseOutput");
 const exchangeRateDisplay = document.getElementById("currencyRate");
 
+/* On page load, if local storage does not contain the list of currencies,
+fetch the list of supported currencies from the API and store it in local storage
+*/
 window.addEventListener("load", async () => {
 	if ("currencies" in localStorage) {
 		const currencies = JSON.parse(localStorage.getItem("currencies"));
@@ -29,6 +37,7 @@ window.addEventListener("load", async () => {
 	}
 });
 
+// Function to add the list of currencies to the select inputs
 function addCurrencyOptions(currencies) {
 	currencies.forEach((currency) => {
 		const optionInput = document.createElement("option");
@@ -42,6 +51,7 @@ function addCurrencyOptions(currencies) {
 	});
 }
 
+// Event listener for the swap button to swap the input and output currencies
 document.getElementById("swap_button").addEventListener("click", () => {
 	const inputCurrency = currencySelectInput.value;
 	const outputCurrency = currencySelectOutput.value;
@@ -56,6 +66,7 @@ document.getElementById("swap_button").addEventListener("click", () => {
 	}
 });
 
+// Function to display and hide the exchange rate between the two selected currencies
 function displayExchangeRate(firstCurrency, secondCurrency, exchangeRate) {
 	if (firstCurrency != "") {
 		const displayedCurrency = 1 * exchangeRate;
@@ -69,12 +80,18 @@ function displayExchangeRate(firstCurrency, secondCurrency, exchangeRate) {
 	}
 }
 
+// Function to handle conversion of the input amount to the output amount
 async function handleAmountInput() {
+	// Get the current time in milliseconds
 	const currentTime = new Date().getTime();
+	// Check if both select inputs have a currency selected
 	if (currencySelectInput.value != "" && currencySelectOutput.value != "") {
+		// Get the first three letters of the selected currencies
 		const inputCurrency = currencySelectInput.value.slice(0, 3);
 		const outputCurrency = currencySelectOutput.value.slice(0, 3);
+		// Check if the selected currencies are different
 		if (inputCurrency !== outputCurrency) {
+			// Check if the exchange rate between the two currencies is already in the list
 			const foundEntry = exchangeRateList.some(
 				(entry) =>
 					(entry.inputCurrency === inputCurrency &&
@@ -83,9 +100,11 @@ async function handleAmountInput() {
 						entry.outputCurrency === inputCurrency)
 			);
 			let timeSinceEntry;
+			// If the exchange rate is in the list, check if it is older than 24 hours
 			if (foundEntry) {
 				timeSinceEntry = currentTime - foundEntry.date;
 			}
+			// If the exchange rate is not in the list or is older than 24 hours, fetch the exchange rate from the API
 			if (!foundEntry || timeSinceEntry > 86400000) {
 				try {
 					const response = await fetch(
@@ -94,6 +113,7 @@ async function handleAmountInput() {
 					const data = await response.json();
 					const exchangeRate = data.conversion_rate;
 
+					// Add the new exchange rate to the list, store it in local storage and add it to exchangeRateList
 					const newEntry = {
 						inputCurrency: inputCurrency,
 						outputCurrency: outputCurrency,
@@ -106,6 +126,8 @@ async function handleAmountInput() {
 						JSON.stringify(exchangeRateList)
 					);
 					currentExchangeRate = exchangeRate;
+
+					// Update the output amount
 					if (amountInput.value != "") {
 						amountOutput.value = (
 							parseFloat(amountInput.value) * exchangeRate
@@ -115,7 +137,8 @@ async function handleAmountInput() {
 					console.error("Error fetching exchange rate:", error);
 				}
 			} else {
-				console.log("Existing entry");
+				//console.log("Existing entry");
+				// If the exchange rate is in the list and is not older than 24 hours, get the exchange rate from the list
 				const entrySame = exchangeRateList.find(
 					(entry) =>
 						(entry.inputCurrency === inputCurrency &&
@@ -124,44 +147,52 @@ async function handleAmountInput() {
 							entry.outputCurrency === inputCurrency)
 				);
 				let exchangeRate;
+				// Check if the input currency is the same as the first currency in the list entry, otherwise invert the exchange rate
 				if (entrySame.inputCurrency === inputCurrency) {
 					exchangeRate = entrySame.exchangeRate;
 				} else {
 					exchangeRate = 1 / entrySame.exchangeRate;
 				}
 				currentExchangeRate = exchangeRate;
+				// Update the output amount
 				amountOutput.value = (
 					parseFloat(amountInput.value) * exchangeRate
 				).toFixed(2);
 			}
 		} else {
-			console.log("Same currency");
+			//console.log("Same currency");
 			currentExchangeRate = 1;
+			// If the input and output currencies are the same, set the output amount to the input amount
 			amountOutput.value = amountInput.value;
 		}
-
+		// Display the exchange rate between the two currencies
 		displayExchangeRate(inputCurrency, outputCurrency, currentExchangeRate);
 	} else {
-		console.log("No currency selected");
+		//console.log("No currency selected");
 		amountOutput.value = "";
+		// If no currency is selected, hide the exchange rate display
 		displayExchangeRate("", "", 0);
 	}
 }
 
+// Event listener to prevent invalid characters in the input field
 amountInput.addEventListener("keypress", function (event) {
 	if (invalidChars.includes(event.key)) {
 		event.preventDefault();
 	}
 });
+// Event listeners for the input fields and select inputs to calculate the output amount
 amountInput.addEventListener("input", handleAmountInput);
 currencySelectInput.addEventListener("change", handleAmountInput);
+currencySelectOutput.addEventListener("change", handleAmountInput);
+
+// Event listeners to add the invalid class to the select inputs if no currency is selected
 currencySelectInput.addEventListener("change", function () {
 	if (currencySelectInput.value == "") {
 		currencySelectInput.classList.add("invalid");
 	} else currencySelectInput.classList.remove("invalid");
 });
 
-currencySelectOutput.addEventListener("change", handleAmountInput);
 currencySelectOutput.addEventListener("change", function () {
 	if (currencySelectOutput.value == "") {
 		currencySelectOutput.classList.add("invalid");
